1a4b86185204ca2e7da88c60c7be53e97d4d5063
David <76574620+chungdavid@users.noreply.github.com>
2026-01-04 20:25:36 -0500

    Add template for projects

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -7,6 +7,16 @@ export default async function(eleventyConfig) {
 
     eleventyConfig.setInputDirectory('src/');
 	eleventyConfig.setOutputDirectory('_site/');
+
+    eleventyConfig.addCollection("projects", function (collectionApi) {
+        return collectionApi
+        .getFilteredByGlob("src/content/projects/*.md")
+        .sort((a, b) => b.date - a.date);
+    });
+
+    eleventyConfig.addFilter("monthYear", function (dateObj) {
+        return dateObj.toLocaleDateString("en-US", {month: "short", year: "numeric"});
+    });
 };
 
 export const config = {
